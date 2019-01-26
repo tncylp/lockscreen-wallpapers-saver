4fdda09d16368943ac8b188df8b3d9d8473cac0e
Tuncay Alp Emekci <tuncayemekci@gmail.com>
2019-01-26 23:58:27 +0300

    Added wallpapers souce

---
+++ src/main/java/test.java
@@ -0,0 +1,10 @@
+import java.io.File;
+
+public class test {
+
+    public static void main(String[] args) {
+
+        File source = new File("C:\\Users\\" + System.getProperty("user.name") + "\\AppData\\Local\\Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\Assets");
+
+    }
+}
